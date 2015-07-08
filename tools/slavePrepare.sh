0c3a9fe99a6ff8f61e248242ab1c5bae5ea96d11
Adrien GILTAIRE <adrien.giltaire@gmail.com>
2015-07-08 20:03:38 +0200

    v1.3

--- tools/slavePrepare.sh
+++ tools/slavePrepare.sh
@@ -12,6 +12,58 @@ sshUsername='root'
 echo "Password"
 read -e -s sshPassword
 
+function copyPartitionTable {
+    echo "================================================================================="
+    echo "Copie de la table de partitions"
+    echo "================================================================================="
+    for i in ${listDisk} ; do
+        sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no "${PREFIX}sfdisk -d ${i}"  | sfdisk ${i}
+    done
+}
+
+function postCopy {
+    echo "================================================================================="
+    echo "Creation des systemes de fichiers"
+    echo "================================================================================="
+    mkfs.ext4 ${bootPart}
+    mkswap ${swapPart}
+
+    echo "================================================================================="
+    echo "Syncro du /boot"
+    echo "================================================================================="
+    mkdir /mnt/boot/
+    mount -t ext4 ${bootPart} /mnt/boot/
+
+    rsync -av --rsh="sshpass -p ${sshPassword} ssh -l ${sshUsername}" ${ipMaster}:/boot/ /mnt/boot/
+    cp -f -v /mnt/boot/initramfs-3.14.19-0.img.slave /mnt/boot/initramfs-3.14.19-0.img
+
+    bootFirstLine=`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}'`
+    bootSecondLine="`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}' | awk -F',' '{print $1}'`)"
+
+    umount ${bootPart}
+
+    echo "================================================================================="
+    echo "Generation du nouveau grub"
+    echo "================================================================================="
+    /sbin/grub --batch <<EOT 1>/dev/null 2>/dev/null
+root ${bootFirstLine}
+setup ${bootSecondLine}
+quit
+EOT
+
+    echo "================================================================================="
+    echo "Replication des partitions LVM"
+    echo "================================================================================="
+    lvmPart=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no pvdisplay | grep -i 'pv name' | awk -F' ' '{print $3}'`
+
+    pvUuid=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no ${PREFIX}lvm pvdisplay | grep -i uuid | awk '{print $3}'`
+    vgName=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no ${PREFIX}lvm vgdisplay | grep -i 'VG NAME' | awk '{print $3}'`
+    rsync -avi --delete --rsh="sshpass -p ${sshPassword} ssh -l ${sshUsername}" ${ipMaster}:/lvmsave/ /etc/lvm/
+    pvcreate --uuid ${pvUuid} --restorefile /etc/lvm/archive/${vgName}_00000* ${lvmPart}
+    vgcfgrestore ${vgName}
+    vgimport ${vgName}
+}
+
 
 listDisk=`ls /dev/[hs]d[a-z]`
 
@@ -31,59 +83,17 @@ echo "==========================================================================
 echo "Indiquer le type de raid utilisé"
 echo "1 - Hardware"
 echo "2 - Software"
-echo "3 - Software - Suite"
 echo "Other Key - Quit"
 read -e raidType
 
-
-lvmCopy=false
-
 case ${raidType} in
     1)
-        echo "================================================================================="
-        echo "Copie de la table de partitions"
-        echo "================================================================================="
-        for i in ${listDisk} ; do
-            sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no "${PREFIX}sfdisk -d ${i}"  | sfdisk ${i}
-        done
+        copyPartitionTable
 
-        echo "================================================================================="
-        echo "Creation des systemes de fichiers"
-        echo "================================================================================="
-        mkfs.ext4 ${bootPart}
-        mkswap ${swapPart}
-
-        echo "================================================================================="
-        echo "Syncro du /boot"
-        echo "================================================================================="
-        mkdir /mnt/boot/
-        mount -t ext4 ${bootPart} /mnt/boot/
-
-        rsync -av --rsh="sshpass -p ${sshPassword} ssh -l ${sshUsername}" ${ipMaster}:/boot/ /mnt/boot/
-        cp -f -v /mnt/boot/initramfs-3.14.19-0.img.slave /mnt/boot/initramfs-3.14.19-0.img
-        
-        bootFirstLine=`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}'`
-        bootSecondLine="`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}' | awk -F',' '{print $1}'`)"
-
-        umount ${bootPart}
-
-        echo "================================================================================="
-        echo "Generation du nouveau grub"
-        echo "================================================================================="
-        /sbin/grub --batch <<EOT 1>/dev/null 2>/dev/null
-root ${bootFirstLine}
-setup ${bootSecondLine}
-quit
-EOT
-        lvmCopy=true
+        postCopy
         ;;
     2)
-        echo "================================================================================="
-        echo "Copie de la table de partitions"
-        echo "================================================================================="
-        for i in ${listDisk} ; do
-            sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no "${PREFIX}sfdisk -d ${i}"  | sfdisk ${i}
-        done
+        copyPartitionTable
 
         # On recupere dans un fichier puis on le supprime:
         # La liste des partition, des types de raid, et le nom des partitions
@@ -129,63 +139,10 @@ EOT
 
         done < /${partitionFileName}
 
-        mdadm --detail --scan --verbose > /etc/mdadm.conf
-
-        ;;
-    3)
-        echo "================================================================================="
-        echo "Creation des systemes de fichiers"
-        echo "================================================================================="
-        mkfs.ext4 ${bootPart}
-        mkswap ${swapPart}
-
-        echo "================================================================================="
-        echo "Syncro du /boot"
-        echo "================================================================================="
-        mkdir /mnt/boot/
-        mount -t ext4 ${bootPart} /mnt/boot/
-
-        rsync -av --rsh="sshpass -p ${sshPassword} ssh -l ${sshUsername}" ${ipMaster}:/boot/ /mnt/boot/
-        cp -f -v /mnt/boot/initramfs-3.14.19-0.img.slave /mnt/boot/initramfs-3.14.19-0.img
-        
-        bootFirstLine=`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}'`
-        bootSecondLine="`cat /mnt/boot/grub/menu.lst | grep -o "(hd[0-9],[0-9])" | awk -v ligne=1 ' NR==ligne {print $1}' | awk -F',' '{print $1}'`)"
-
-        umount ${bootPart}
-
-        echo "================================================================================="
-        echo "Generation du nouveau grub"
-        echo "================================================================================="
-        /sbin/grub --batch <<EOT 1>/dev/null 2>/dev/null
-root ${bootFirstLine}
-setup ${bootSecondLine}
-quit
-EOT
-        lvmCopy=true
+        postCopy
         ;;
     *)
         echo "Bye !"
         exit 0
         ;;
-
-
 esac
-
-if ${lvmCopy} ; then
-    echo "================================================================================="
-    echo "Replication des partitions LVM"
-    echo "================================================================================="
-    lvmPart=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no pvdisplay | grep -i 'pv name' | awk -F' ' '{print $3}'`
-    
-    pvUuid=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no ${PREFIX}lvm pvdisplay | grep -i uuid | awk '{print $3}'`
-    vgName=`sshpass -p "${sshPassword}" ssh ${sshUsername}@${ipMaster} -o StrictHostKeyChecking=no ${PREFIX}lvm vgdisplay | grep -i 'VG NAME' | awk '{print $3}'`
-    rsync -avi --delete --rsh="sshpass -p ${sshPassword} ssh -l ${sshUsername}" ${ipMaster}:/lvmsave/ /etc/lvm/
-    pvcreate --uuid ${pvUuid} --restorefile /etc/lvm/archive/${vgName}_00000* ${lvmPart}
-    vgcfgrestore ${vgName}
-    vgimport ${vgName}
-fi
-
-
-
-
-
