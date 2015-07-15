b8ae7ca9e21f1e29e4f24106d4d252b86d49778f
Adrien GILTAIRE <adrien.giltaire@gmail.com>
2015-07-15 16:34:44 +0200

    v1.3

--- 90drbd/boot.sh
+++ 90drbd/boot.sh
@@ -30,8 +30,10 @@ unset error
 
 if [[ $role == "master" ]] ; then
     hostname=$hostname_master
+    ip=${ip_master}
 else
     hostname=$hostname_slave
+    ip=${ip_slave}
 fi
 
 ######################################################
@@ -74,9 +76,32 @@ else
     mount -t ext4 ${DRBD_ROOT} /sysroot
     returnCode=$?
     sleep 2
+
+    # Modification du HOSTNAME au demarrage
     sed -i /"HOSTNAME="/d /sysroot/etc/sysconfig/network
     echo "HOSTNAME=${hostname}" > /sysroot/etc/sysconfig/network
+
+    # Suppression des rules qui font chier
     rm -f /sysroot/etc/udev/rules.d/70-persistent-net.rules
+
+    # Suppression de l'UUID dans le fichier de conf reseau
+    sed -i /"UUID="/d /sysroot/etc/sysconfig/networking/devices/ifcfg-${devName}
+    sed -i /"UUID="/d /sysroot/etc/sysconfig/network-scripts/ifcfg-${devName}
+
+    # Modification de la MAC au demarrage
+    sed -i /"HWADDR="/d /sysroot/etc/sysconfig/networking/devices/ifcfg-${devName}
+    sed -i /"HWADDR="/d /sysroot/etc/sysconfig/network-scripts/ifcfg-${devName}
+    hwAddr=`cat /sys/class/net/${devName}/address | awk '{print toupper($1)}'`
+    echo "HWADDR=${hwAddr}" >> /sysroot/etc/sysconfig/networking/devices/ifcfg-${devName}
+    echo "HWADDR=${hwAddr}" >> /sysroot/etc/sysconfig/network-scripts/ifcfg-${devName}
+
+    # Modification de l'IP au demarrage
+    sed -i /"IPADDR="/d /sysroot/etc/sysconfig/networking/devices/ifcfg-${devName}
+    sed -i /"IPADDR="/d /sysroot/etc/sysconfig/network-scripts/ifcfg-${devName}
+    echo "IPADDR=${ip}" >> /sysroot/etc/sysconfig/networking/devices/ifcfg-${devName}
+    echo "IPADDR=${ip}" >> /sysroot/etc/sysconfig/network-scripts/ifcfg-${devName}
+
+
     /bin/rm -f /dev/root
     /bin/ln -s ${DRBD_ROOT} /dev/root
     if [[ ${returnCode} == 0 ]] ; then
