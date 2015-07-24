de851b73bab125ad32aee177129afec8c946ddf3
Adrien GILTAIRE <adrien.giltaire@gmail.com>
2015-07-24 14:06:57 +0200

    v1.4

--- checkApp.sh
+++ checkApp.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+. ./config
+
 # Sans ces packages / cmd l'install ne demarre pas
-NEEDED_COMMANDS="dropbear gcc snmpd sshpass"
-NEEDED_PACKAGES="dracut-network"
+NEEDED_COMMANDS=""
+NEEDED_PACKAGES=""
 
 error=0
 dropbearkey='dropbearkey'
+
+for i in ${modToInstall} ; do
+    case ${i} in
+        88snmp)
+            echo ${NEEDED_PACKAGES} | grep "dracut-network" 1>&2 > /dev/null
+            if [ $? -eq 1 ] ; then
+                NEEDED_PACKAGES="${NEEDED_PACKAGES} dracut-network"
+            fi
+            NEEDED_COMMANDS="${NEEDED_COMMANDS} snmpd"
+            ;;
+        89dropbear)
+            echo ${NEEDED_PACKAGES} | grep "dracut-network" 1>&2 > /dev/null
+            if [ $? -eq 1 ] ; then
+                NEEDED_PACKAGES="${NEEDED_PACKAGES} dracut-network"
+            fi
+            NEEDED_PACKAGES="${NEEDED_PACKAGES} dracut-network"
+            NEEDED_COMMANDS="${NEEDED_COMMANDS} dropbear"
+            ;;
+        90drbd)
+            echo ${NEEDED_PACKAGES} | grep "dracut-network" 1>&2 > /dev/null
+            if [ $? -eq 1 ] ; then
+                NEEDED_PACKAGES="${NEEDED_PACKAGES} dracut-network"
+            fi
+            NEEDED_COMMANDS="${NEEDED_COMMANDS} sshpass rsync bc"
+            ;;
+        91python)
+            NEEDED_COMMANDS="${NEEDED_COMMANDS} python"
+            ;;
+    esac
+done
 
 for cmd in ${NEEDED_COMMANDS} ; do
     if ! command -v ${cmd} &> /dev/null ; then
@@ -13,21 +45,6 @@ for cmd in ${NEEDED_COMMANDS} ; do
         error=$((error+1))
     fi
 done
-
-if [[ ! -d /etc/dracut-dropbear/ ]] ; then
-    mkdir /etc/dracut-dropbear/
-fi
-
-if [[ ! -e /etc/dracut-dropbear/dropbear_rsa_host_key ]] ; then
-    if command -v ${dropbearkey} $> /dev/null ; then
-        dropbearkey -t rsa -s 3072 -f /etc/dracut-dropbear/dropbear_rsa_host_key
-        echo "Cle RSA OK"	
-    else 
-        echo "Cle RSA non installee"
-        error=$((error+1))
-    fi
-fi
-
 
 for pkg in ${NEEDED_PACKAGES} ; do
     if ! rpm -q ${pkg} &> /dev/null ; then
@@ -41,5 +58,25 @@ if [[ ${error} > 1 ]] ; then
     echo "Si vous savez ce que vous faites vous pouvez editer la liste des commandes requises dans checkApp.sh"
 	exit 1
 fi
+
+for i in ${modToInstall} ; do
+    case ${i} in
+        89dropbear)
+            if [[ ! -d /etc/dracut-dropbear/ ]] ; then
+                mkdir /etc/dracut-dropbear/
+            fi
+            if [[ ! -e /etc/dracut-dropbear/dropbear_rsa_host_key ]] ; then
+                if command -v ${dropbearkey} $> /dev/null ; then
+                    dropbearkey -t rsa -s 3072 -f /etc/dracut-dropbear/dropbear_rsa_host_key
+                    echo "Cle RSA OK"	
+                else 
+                    echo "Cle RSA non installee"
+                    echo "L'installation ne peut pas continuer"
+                    exit 1
+                fi
+            fi
+            ;;
+    esac
+done
 
 touch .cmd_ok
